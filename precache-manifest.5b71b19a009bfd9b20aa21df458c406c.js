153abe8be6a0940768e75f831ce1d5d285f278f7
haritharu44 <harichaudhary3022@gmail.com>
2022-10-09 17:39:21 +0545

    Updates

---
+++ precache-manifest.5b71b19a009bfd9b20aa21df458c406c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d202dc475836bdc50088",
+    "url": "/TextForm/static/css/main.a5cc5a7e.chunk.css"
+  },
+  {
+    "revision": "d202dc475836bdc50088",
+    "url": "/TextForm/static/js/main.d202dc47.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextForm/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextForm/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "fea397d40a427d03062f",
+    "url": "/TextForm/static/js/runtime~main.fea397d4.js"
+  },
+  {
+    "revision": "9793155c60c1ad50af93929132c5af0b",
+    "url": "/TextForm/index.html"
+  }
+];
